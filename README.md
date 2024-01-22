d9ad6500768f07aa1f94d5061b2694e5a7708ed0
Capella87 <contact@capelladev.com>
2024-01-22 23:56:50 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# PSScripts
+
+A collection of convenient PowerShell scripts
+
+## Catalogs
+
+### Batch works
+
+* robocopy-script.ps1 : Copy directories or files with elegant Robocopy.
---
+++ robocopy-script.ps1
@@ -0,0 +1,109 @@
+param
+(
+    [Parameter(Mandatory=$true)]
+    [string]$source,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$destination,
+    
+    [Parameter(Mandatory=$false)]
+    [string]$logFileLocation = $null,
+
+    [Parameter(Mandatory=$false)]
+    [switch]$NewDestSubDir = $false,
+    
+    [Parameter(Mandatory=$false)]
+    [switch]$SameSrcNameDir = $false,
+
+    [Parameter(Mandatory=$false)]
+    [switch]$OpenDestDirAfterCopy = $false
+
+    # [Parameter(Mandatory=$false)]
+    # [switch]$RemoveSrcDirAfterCopy = $false
+)
+
+if ($NewDestSubDir -and $SameSrcNameDir)
+{
+    Write-Host "Cannot use both NewDestSubDir and SameSrcNameDir switches."
+    exit
+}
+
+$timestamp = Get-Date -Format 'yyyyMMdd_HHmmss'
+
+if (!(Test-Path -Path $source))
+{
+    Write-Host "Source path does not exist."
+    exit
+}
+
+# if (!(Test-Path -Path $destination) -and $NewDestSubDir -eq $false)
+# {
+#     Write-Host "Destination path does not exist."
+#     exit
+# }
+# elseif ((Test-Path -Path $destination) -and $NewDestSubDir)
+# {
+#     New-Item -Type Directory -Path $destination
+# }
+
+if (!(Test-Path -Path $destination))
+{
+    New-Item -Type Directory -Path $destination
+}
+
+if ($NewDestSubDir)
+{
+    $destination += "\copied_$($timestamp)\"
+    New-Item -Type Directory -Path $destination
+}
+Write-Debug "Destination: $($destination)"
+
+if ($SameSrcNameDir)
+{
+    $destination += "\$((Get-Item $source).Name)"
+    New-Item -Type Directory -Path $destination
+}
+
+if ($logfileLocation -eq "")
+{
+    $logFileLocation = "$($destination)\log_$($timestamp).txt"
+}
+
+Write-Host $logFileLocation
+
+## TODO: We need to workaround log file encoding bug in RoboCopy
+
+Write-Host "Start copying $($source) to $($destination) with log file $($logFileLocation)"
+
+Robocopy.exe $source $destination *.* `
+    /e `
+    /zb `
+    /j `
+    /copy:DATS `
+    /dcopy:DATE `
+    /a-:R `
+    /pf `
+    /mt:8 `
+    /xa:ST `
+    /xd "System Volume Information" `
+    /r:1000000 `
+    /w:300 `
+    /v `
+    /ts `
+    /eta `
+    /unilog:"$($logFileLocation)" `
+    /tee `
+    /unicode
+
+# Re-save Log file to UTF-8
+Write-Debug "Opening $($logFileLocation) for UTF-8 without BOM re-encoding."
+$logFileContent = Get-Content -Path $sourceFile -Encoding unicode
+
+Write-Debug "Saving $($logFileLocation) with UTF-8 (No BOM)"
+$logFileContent | Set-Content -FilePath $sourceFile -Encoding utf8NoBOM
+
+if ($OpenDestDirAfterCopy)
+{
+    Write-Debug "Opening $($destination)"
+    Start-Process $destination
+}
